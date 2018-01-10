d091ad4a55b242851b633e636e4843954b6de90c
Auston Stewart <auston@shedbuilt.net>
2018-01-10 02:46:32 -0500

    Updating to 3.1.6

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 ./configure  --prefix=/usr        \
              --disable-static     \
              --enable-thread-safe \
-             --docdir=/usr/share/doc/mpfr-3.1.5
-make -j $SHED_NUMJOBS
-make html
-make DESTDIR=${SHED_FAKEROOT} install
-make DESTDIR=${SHED_FAKEROOT} install-html
+             --docdir=/usr/share/doc/mpfr-3.1.6 || return 1
+make -j $SHED_NUMJOBS || return 1
+make html || return 1
+make DESTDIR="$SHED_FAKEROOT" install || return 1
+make DESTDIR="$SHED_FAKEROOT" install-html || return 1
