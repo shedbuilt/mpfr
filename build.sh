a9156bb31039185d9364fff08f4dda978e369a19
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:26:27 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
              --disable-static     \
              --enable-thread-safe \
              --docdir=/usr/share/doc/mpfr-4.0.1 && \
-make -j $SHED_NUMJOBS && \
+make -j $SHED_NUM_JOBS && \
 make html && \
-make DESTDIR="$SHED_FAKEROOT" install && \
-make DESTDIR="$SHED_FAKEROOT" install-html || exit 1
+make DESTDIR="$SHED_FAKE_ROOT" install && \
+make DESTDIR="$SHED_FAKE_ROOT" install-html || exit 1
