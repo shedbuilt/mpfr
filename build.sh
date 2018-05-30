dca36944a754ed21dcf026265881da14ad43291b
Auston Stewart <auston@shedbuilt.net>
2018-05-29 19:27:07 -1000

    Adding build options

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VESION}"
 ./configure  --prefix=/usr        \
              --disable-static     \
              --enable-thread-safe \
-             --docdir=/usr/share/doc/mpfr-4.0.1 && \
-make -j $SHED_NUM_JOBS && \
-make html && \
-make DESTDIR="$SHED_FAKE_ROOT" install && \
-make DESTDIR="$SHED_FAKE_ROOT" install-html || exit 1
+             --docdir=${SHED_PKG_LOCAL_DOCDIR} &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    make html &&
+    make DESTDIR="$SHED_FAKE_ROOT" install-html || exit 1
+else
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
